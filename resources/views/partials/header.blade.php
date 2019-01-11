1d23916e2a9051d4fa737ecf83eb137ffb406a7d
gabzon <gab.zambrano@gmail.com>
2019-01-11 14:14:38 +0100

    mobile menu icon fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,8 +8,8 @@
         <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/dancefloor-logo-white.svg" alt="Dancefloor logo" class="img-fluid" width="150"/>
       @endif
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">      
+      <i class="fas fa-bars"></i>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
       @if (has_nav_menu('primary_navigation'))
